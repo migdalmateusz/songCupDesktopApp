c8ee7f71b40364cc86c5f310b83cd719ac041c28
Mateusz <migdalmateusz@wp.pl>
2021-03-25 08:51:53 +0100

    create package spotifyapi, create ClientCredentials class

---
+++ src/main/java/songcup/spotifyapi/ClientCredentialsExample.java
@@ -0,0 +1,38 @@
+package songcup.spotifyapi;
+
+import com.wrapper.spotify.SpotifyApi;
+import com.wrapper.spotify.exceptions.SpotifyWebApiException;
+import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
+import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+import org.apache.hc.core5.http.ParseException;
+
+import java.io.IOException;
+
+public class ClientCredentialsExample {
+    private static final String clientId = "d483a7a819dd4b729196977cf28b8002";
+    private static final String clientSecret = "c0d3fcf56b884cd6b138a45b6b8f6e10";
+
+    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+            .setClientId(clientId)
+            .setClientSecret(clientSecret)
+            .build();
+    private static final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+            .build();
+
+    public static void clientCredentials_Sync() {
+        try {
+            final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
+
+            // Set access token for further "spotifyApi" object usage
+            spotifyApi.setAccessToken(clientCredentials.getAccessToken());
+
+            System.out.println("Expires in: " + clientCredentials.getExpiresIn());
+        } catch (IOException | SpotifyWebApiException | ParseException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        clientCredentials_Sync();
+    }
+}
